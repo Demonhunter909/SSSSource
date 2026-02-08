71d39bdb88be00bb45c873edfb23eae53010efb6
Demonhunter909 <Brandonbarbee512@gmail.com>
2026-02-08 22:03:51 +0000

    attempted fix x3

--- app.py
+++ app.py
@@ -234,27 +234,27 @@ def articles():
     return render_template("articles.html", files=files)
 
 @app.route("/venom")
-def venom_page():
+def venom():
     return render_template("venom.html", files=files)
 
 @app.route("/talent")
-def talent_page():
+def talent():
     return render_template("talent.html", files=files)
 
 @app.route("/athletics")
-def athletics_page():
+def athletics():
     return render_template("athletics.html", files=files)
 
 @app.route("/entertainment")
-def entertainment_page():
+def entertainment():
     return render_template("entertainment.html", files=files)
 
 @app.route("/news")
-def news_page():
+def news():
     return render_template("news.html", files=files)
 
 @app.route("/features")
-def features_page():
+def features():
     return render_template("features.html", files=files)
 
 if __name__ == '__main__':
