2ebac2df11e5aa7bf2b2fb0535703834cfb99b23
Demonhunter909 <Brandonbarbee512@gmail.com>
2026-02-17 01:06:43 +0000

    Fix attempt 1 hours undefined

--- utils.py
+++ utils.py
@@ -4,5 +4,5 @@ import psycopg2
 def generate_token():
     return secrets.token_hex(32)
 
-def token_expiration():
+def token_expiration(hours=24):
     return datetime.datetime.utcnow() + datetime.timedelta(hours=hours)
